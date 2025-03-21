1aad4f4a587a980445e7cd8256402eb25d297854
Harshit Rastogi <harshitrastogi0206@gmail.com>
2025-03-21 22:18:39 +0530

    created awards component

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 const Awards = () => {
   return (
-    <div>
-      <h1>Awards</h1>
+    <div className="container mt-5 mb-5">
+      <div className="row">
+        <div className="col-6">
+          <img src="images/largestBroker.svg" alt="Awards_image" />
+        </div>
+        <div className="col-6 mt-5  p-5">
+          <h1>Largest Stock Broker of India</h1>
+          <p className="mt-3 mb-2">2+ million Zerodha clients contribute to  over 15% of all retail order volumes of India daily by trading and investing: </p>
+          <div className="row">
+            <div className="col-6 p-5">
+            <ul>
+              <li> <p>Futures and Options</p></li>
+              <li> <p>Commodity derivatives</p></li>
+              <li> <p>Currency Derivatives</p></li>
+            </ul>
+            </div>
+            <div className="col-6 p-5">
+            <ul>
+              <li> <p>Stocks and IPOs</p></li>
+              <li> <p>Direct Mutual Funds</p></li>
+              <li> <p>Bonds and Govt. Security</p></li>
+            </ul>
+            </div>
+            <img src="images/pressLogos.png" alt="press_image" style={{width:"95%"}}/>
+          </div>
+          
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      <h1>hero-section</h1>
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img src='images/homeHero.png' alt="Hero-image" className="mb-5"/>
+        <h1 className="mt-5">Invest in everything.</h1>
+        <p>Online platform to invest in stocks , derivatives , mutual funds , and more .</p>
+        <button className="p-2 btn btn-primary fs-5" style={{width:"18%", margin:"0 auto" }}>Signup Now</button>
+      </div>
     </div>
   )
 }
