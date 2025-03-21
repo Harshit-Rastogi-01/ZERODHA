e6c1c9490f8e77c0df98d3bfa2b29d330b106348
Harshit Rastogi <harshitrastogi0206@gmail.com>
2025-03-21 22:56:24 +0530

    created stats component

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 const Stats = () => {
   return (
-    <div>
-      <h1>Stats</h1>
+    <div className="container p-3">
+      <div className="row p-5">
+        <div className="col-6 p-5">
+          <h1 className="mb-5">Trust with Confidence</h1>
+          <h2 className="fs-4">Customer-first always</h2>
+          <p className="text-muted">That's why 1.3+ crore customers trust Zerodha with  <i class="fa-solid fa-indian-rupee-sign"></i> 3.5+ lakh crore worth of equity investments .</p>
+          <h2 className="fs-4">No spam or gimmicks</h2>
+          <p className="text-muted">No gimmicks , spam , "gamifiaction" , or annoying push notfications. high quality apps that you use at your pace , the way ypu like.</p>
+          <h2 className="fs-4">The Zerodha universe</h2>
+          <p className="text-muted">Not just an app , but a whole ecosystem . Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+          <h2 className="fs-4">Do better with money</h2>
+          <p className="text-muted">With initiaitves like Nudge and Kill Switch , we don;t just facilitate transactions , but actively help you do better with your money</p>
+          </div>
+        <div className="col-6 p-5"> 
+          <img src="images/ecosystem.png" style={{width:"90%"}} alt="ecosystem_image"/>
+          <div className="text-center">
+            <a href='' className="mx-4" style={{textDecoration:"none"}}>Explore our products <i class="fa-solid fa-arrow-right"></i></a>
+            <a href='' style={{textDecoration:"none"}}>Try kite <i class="fa-solid fa-arrow-right"></i></a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
